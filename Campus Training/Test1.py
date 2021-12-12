f25c1c331e0a984f91d72481250b6119f2390e45
Ayan <imeayan2000@gmail.com>
2021-12-12 14:53:59 +0530

    new commit

--- Campus Training/Test1.py
+++ Campus Training/Test1.py
@@ -1,3 +1,4 @@
+'''
 name='Ayan Kumar Maji'
 age=22
 print(f"My name is {name} and I'm {age} years old")
@@ -7,5 +8,36 @@ for i in range(1,n+1):
     f=f*i
 
 print(f"Factorial of {n} is {f}")
+
+'''
+'''
+n=int(input("Enter a number: "))
+i=1
+while i <= 10:
+    print(f"{n}X{i}={n*i}")
+    i+=1
+
+'''
+
+'''
+n=int(input("Enter the number : "))
+c=0
+for i in range(1,n+1):
+    if n%i==0:
+        c+=1
+
+if c==2:
+        print(f"{n} is a prime number")
+else:
+        print(f"{n} is not prime number")
+
+'''
+
+num=int(input("Enter a number : "))
+s=0
+for i in range(1,num+1):
+    s=s+i
+
+print(f"Sum of first {num} natural nos is {s}")
 
 
